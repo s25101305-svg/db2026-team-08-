fb0930a905f5237c0c77c8fd0c8a385b36897934
Mingjie05 <s25101305@stu.edu.tw>
2026-06-08 17:00:56 +0800

    Add files via upload

---
+++ UniversitySystemManagement2026/AdminForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversitySystemManagement2026
+{
+    public partial class AdminForm : Form
+    {
+        public AdminForm()
+        {
+            InitializeComponent();
+        }
+
+        private void userBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.userBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.universityDataSet1);
+
+        }
+
+        private void userBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.userBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.universityDataSet1);
+
+        }
+
+        private void userBindingNavigatorSaveItem_Click_2(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.userBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.universityDataSet1);
+
+        }
+
+        private void userBindingNavigatorSaveItem_Click_3(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.userBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.universityDataSet1);
+
+        }
+
+        private void userBindingNavigatorSaveItem_Click_4(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.userBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.universityDataSet1);
+
+        }
+
+        private void AdminForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'universityDataSet.department' table. You can move, or remove it, as needed.
+            this.departmentTableAdapter.Fill(this.universityDataSet.department);
+            // TODO: This line of code loads data into the 'universityDataSet.course' table. You can move, or remove it, as needed.
+            this.courseTableAdapter.Fill(this.universityDataSet.course);
+            // TODO: This line of code loads data into the 'universityDataSet1.user' table. You can move, or remove it, as needed.
+            this.userTableAdapter.Fill(this.universityDataSet1.user);
+
+        }
+    }
+}
